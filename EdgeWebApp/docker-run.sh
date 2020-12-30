b9e15172bc8ce74273abc0a4fef26d2a364588f6
trilok.rangan <trilokdude@gmail.com>
2020-12-30 11:30:22 +0530

    endpoint changes
    publishing end points and port bindings are changed

--- EdgeWebApp/docker-run.sh
+++ EdgeWebApp/docker-run.sh
@@ -5,8 +5,10 @@ docker rm -f edgewebapp 2>/dev/null
 docker run \
         -d \
         --name edgewebapp \
-        -p 5001:443 \
-        -p 5000:80 \
+        -p 443:443 \
+        -p 80:80 \
+        -e ASPNETCORE_HTTPS_PORT=443 \
+        -e ASPNETCORE_URLS='https://+;http://+' \
         -e ASPNETCORE_Kestrel__Certificates__Default__Path='EdgeWebApp.pfx' \
         -e ASPNETCORE_Kestrel__Certificates__Default__Password='P@ssw0rd1!' \
-        bellwindcontainer.azurecr.io/edgewebappmactri:0.0.10
+        bellwindcontainer.azurecr.io/edgewebappmactri:0.0.19
